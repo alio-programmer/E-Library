6688c61150cc2abe82655c8114c4bd237fb4d1b9
Gaurav singh bisht <gauravbisht82754@gmail.com>
2024-07-21 16:37:16 +0530

    basic setup

---
+++ Backend/Config/DB.js
@@ -0,0 +1,20 @@
+import mongoose from "mongoose";
+
+const connectDB = async () => {
+  try {
+    mongoose.connection.on("connected", () => {
+      console.log("Database connected successfully");
+    });
+
+    mongoose.connection.on("error", () => {
+      console.log("Error in connecting to database");
+    });
+
+    await mongoose.connect("mongodb://localhost:27017/libname");
+  } catch (error) {
+    console.log(error.message);
+    process.exit(1);
+  }
+};
+
+export default connectDB;
---
+++ Backend/index.js
@@ -0,0 +1,24 @@
+import express from "express";
+import dotenv from "dotenv";
+import connectDB from "./config/DB.js";
+dotenv.config();
+const app = express();
+
+app.use(express.json());
+
+const startserver = async () => {
+  await connectDB();
+
+  try {
+    app.listen(process.env.PORT || 5050, () => {
+      console.log(`server listening on http://localhost:${process.env.PORT}`);
+    });
+  } catch (error) {
+    console.log(error.message);
+  }
+};
+startserver();
+
+app.get("/", (req, res) => {
+  res.send("API is running");
+});
